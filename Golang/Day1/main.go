ec779133e957a70e3a53b4efb70a0ed623115f6c
luminous479 <aklemasultanakhi@gmail.com>
2026-01-06 11:59:28 +0600

    variable declaration

--- Golang/Day1/main.go
+++ Golang/Day1/main.go
@@ -6,4 +6,15 @@ func main() {
 
 	fmt.Println("hello world!")
 
+	// variable declaration 
+	var a int = 10
+    fmt.Println(a)
+	 b := 5
+	fmt.Println(b)
+	b = 15
+	fmt.Println(b)
+	const p = 100;
+    fmt.Println(p)
+
+
 }
