5c17acc1bf1f11c23bbd975d88d48061edb03a2e
luminous479 <aklemasultanakhi@gmail.com>
2026-02-09 22:51:14 +0600

    react jsx

---
+++ React/my/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+     <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+     </nav>
+     <div className="container">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+       Ad quasi magnam autem esse aliquid atque ea tenetur deserunt voluptatum voluptates.
+     </div>
+    </>
+  
+  );
+}
+
+export default App;
