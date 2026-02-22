85f2e2f21e5f0523a4fabb976249dead553997a6
luminous479 <aklemasultanakhi@gmail.com>
2026-02-22 21:32:09 +0600

    data type and veriable in python

---
+++ Python/DataTypeAndVariables.py
@@ -0,0 +1,17 @@
+print("Welcome to Python Programming")
+name = "John"
+age = 30 
+print("My name is", name, "and I am", age, "years old.")
+# Data Types
+# String
+greeting = "Hello, World!"
+print(greeting)
+# Integer
+year = 2024
+print(year)
+# Float
+pi = 3.14159
+print(pi)
+# Boolean
+is_sunny = True
+print(is_sunny)
