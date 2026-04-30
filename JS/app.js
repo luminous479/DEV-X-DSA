618ab807c2a28f72a9b3e85610f27f4d04c2c157
luminous479 <aklemasultanakhi@gmail.com>
2026-04-30 15:13:11 +0600

    Create a comment box and display comment

--- JS/app.js
+++ JS/app.js
@@ -1,31 +1,48 @@
 
-const element = document.querySelector('h1');
-element.style.color = 'red';
-element.style.fontSize = '24px';
-element.style.fontWeight = 'bold';
-element.style.textAlign = 'center';
-element.style.textShadow = '2px 2px 4px rgba(0, 0, 0, 0.5)';
+// const element = document.querySelector('h1');
+// element.style.color = 'red';
+// element.style.fontSize = '24px';
+// element.style.fontWeight = 'bold';
+// element.style.textAlign = 'center';
+// element.style.textShadow = '2px 2px 4px rgba(0, 0, 0, 0.5)';
 
-const button = document.getElementById('btn');
-button.onclick = function() {
-   document.body.style.backgroundColor = 'lightblue';
-}
+// const button = document.getElementById('btn');
+// button.onclick = function() {
+//    document.body.style.backgroundColor = 'lightblue';
+// }
 
-const greenButton = document.getElementById('green-btn');
-greenButton.addEventListener('click', function() {
-    document.body.style.backgroundColor = 'lightgreen';
-});
+// const greenButton = document.getElementById('green-btn');
+// greenButton.addEventListener('click', function() {
+//     document.body.style.backgroundColor = 'lightgreen';
+// });
 
-const dtextButton = document.getElementById('dtext-btn');
-dtextButton.addEventListener('click', function() {
-    const dtext = document.getElementById('dtext');
-    dtext.textContent = 'Text has been changed!';
-});
+// const dtextButton = document.getElementById('dtext-btn');
+// dtextButton.addEventListener('click', function() {
+//     const dtext = document.getElementById('dtext');
+//     dtext.textContent = 'Text has been changed!';
+// });
 
-const inputButton = document.getElementById('input-btn');
-inputButton.addEventListener('click', function() {
-    const inputText = document.getElementById('input-text');
-    const dtext = document.getElementById('dtext');
-    dtext.textContent = inputText.value
-    inputText.value = '';
+// const inputButton = document.getElementById('input-btn');
+// inputButton.addEventListener('click', function() {
+//     const inputText = document.getElementById('input-text');
+//     const dtext = document.getElementById('dtext');
+//     dtext.textContent = inputText.value
+//     inputText.value = '';
+// });
+
+
+
+const cmntContainer = document.getElementById('comment-container');
+
+const commentInput = document.getElementById('comment-input');
+const submitButton = document.getElementById('submit-btn');
+
+submitButton.addEventListener('click', function() {
+    const commentText = commentInput.value.trim();
+    if (commentText !== '') {
+        const commentElement = document.createElement('p');
+        commentElement.textContent = commentText;
+        cmntContainer.appendChild(commentElement);
+        commentInput.value = '';
+    }
 });
